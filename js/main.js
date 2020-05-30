59a9b0d1b73941d727e3b53a38a7cbd5a7c1793f
tk <kmnswimert@gmail.com>
2020-05-30 16:13:32 +0900

    chore(js): :pill:  temporary

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
 $(function(){
-    $('a[href^="#"]').click(function(){
-        var time = 500;
-        var href= $(this).attr("href");
-        var target = $(href == "#" ? 'html' : href);
-        var distance = target.offset().top;
-        $('body,html').animate({scrollTop:distance}, time, "swing");
-        console.log("test")
-        return false;
+	$('a[href^=#w]').click(function clickworks(){
+            return new Promise(function(resolve, reject){
+            var scroll = document.getElementById( "scroll" );
+            var speed = 1000;
+            var position = 701
+            $("scroll").removeClass("scroll-snap")
+            // scroll.classList.remove("scroll-snap");
+            // console.log(scroll);
+            $(".container").animate({scrollTop:position}, speed, "swing");
+            resolve();
+            return false;
+        });
+        // console.log(scroll);
+        async function doworks(){
+            var result = await clickworks();
+            var scroll = document.getElementById( "scroll" );
+            $("scroll").addClass("scroll-snap")
+        }
+        doworks();
     });
-  });
+    $('a[href^=#a]').click(function(){
+        var speed = 1000;
+        var position = 1401
+        console.log(position);
+        $("scroll").removeClass("scroll-snap")
+        $(".container").animate({scrollTop:position}, speed, "swing");
+        $("scroll").addClass("scroll-snap")
+		return false;
+    });
+    $('a[href^=#s]').click(function(){
+        var speed = 1000;
+        var position = 2101
+        console.log(position);
+        $("scroll").removeClass("scroll-snap")
+        $(".container").animate({scrollTop:position}, speed, "swing");
+        $("scroll").addClass("scroll-snap")
+		return false;
+	});
+});
