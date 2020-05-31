688105ec579ef31926cfc6faf3efa89bc43a850a
tk <kmnswimert@gmail.com>
2020-05-31 18:01:06 +0900

    chore(js): :pill: スクロール移動制御

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
 $(function(){
-	$('a[href^=#w]').click(function clickworks(){
-            return new Promise(function(resolve, reject){
-            var scroll = document.getElementById( "scroll" );
-            var speed = 1000;
-            var position = 701
-            $("scroll").removeClass("scroll-snap")
-            // scroll.classList.remove("scroll-snap");
-            // console.log(scroll);
-            $(".container").animate({scrollTop:position}, speed, "swing");
-            resolve();
-            return false;
+	$('a[href^=#w]').click(function(){
+        var scroll = document.getElementById( "scroll" );
+        var speed = 1000;
+        var position = 701;
+        scroll.classList.remove("scroll-snap");
+        $(".container").stop().animate({scrollTop:position}, speed, "swing",function(){
+            scroll.classList.add("scroll-snap");
         });
-        // console.log(scroll);
-        async function doworks(){
-            var result = await clickworks();
-            var scroll = document.getElementById( "scroll" );
-            $("scroll").addClass("scroll-snap")
-        }
-        doworks();
+        return false;
     });
     $('a[href^=#a]').click(function(){
+        var scroll = document.getElementById( "scroll" );
         var speed = 1000;
         var position = 1401
-        console.log(position);
-        $("scroll").removeClass("scroll-snap")
-        $(".container").animate({scrollTop:position}, speed, "swing");
-        $("scroll").addClass("scroll-snap")
+        scroll.classList.remove("scroll-snap");
+        $(".container").stop().animate({scrollTop:position}, speed, "swing",function(){
+            scroll.classList.add("scroll-snap");
+        });
 		return false;
     });
     $('a[href^=#s]').click(function(){
+        var scroll = document.getElementById( "scroll" );
         var speed = 1000;
         var position = 2101
-        console.log(position);
-        $("scroll").removeClass("scroll-snap")
-        $(".container").animate({scrollTop:position}, speed, "swing");
-        $("scroll").addClass("scroll-snap")
+        scroll.classList.remove("scroll-snap");
+        $(".container").stop().animate({scrollTop:position}, speed, "swing",function(){
+            scroll.classList.add("scroll-snap");
+        });
 		return false;
 	});
 });
